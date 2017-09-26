76392a060b75ca3a9c67863859513d333c31aad1
Lucas Lavandeira <lucaslavandeira@gmail.com>
2017-09-26 15:23:18 -0300

    Update a requirements/setup

--- setup.py
+++ setup.py
@@ -6,14 +6,10 @@ with open(os.path.abspath("requirements.txt")) as f:
     requirements = [req.strip() for req in f.readlines()]
 
 setup(
-    name='pydatajson_ts',
-    version='dev',
-    packages=[
-        'pydatajson_ts'
-    ],
-    package_dir={'pydatajson_ts': 'pydatajson_ts'},
-    include_package_data=True,
+    name='pydatajson-ts',
+    version='0.0.1',
+    packages=['pydatajson_ts'],
+    package_dir={'pydatajson_ts':'pydatajson_ts'},
     zip_safe=False,
     install_requires=requirements,
-
 )
