1a3304ac39ed315bdf99d50e03b5bee2e63b356a
Agustín Benassi <agusbenassi@gmail.com>
2019-01-25 18:18:24 -0300

    Agrego funciones para lidiar con la detección de diferentes métodos de generación de distribuciones.

--- series_tiempo_ar/readers.py
+++ series_tiempo_ar/readers.py
@@ -15,9 +15,12 @@ import os
 import pandas as pd
 
 from helpers import freq_iso_to_pandas, find_encoding, find_dialect
+from pydatajson.time_series import get_distribution_time_index
 
 
-def load_ts_distribution(catalog, identifier, catalog_id=None):
+def load_ts_distribution(catalog, identifier, catalog_id=None,
+                         is_text_file=None, is_excel_file=None,
+                         is_csv_file=None, file_source=None):
     """Carga en un DataFrame una distribución de series de tiempo.
 
     Permite leer (online o local):
@@ -25,26 +28,74 @@ def load_ts_distribution(catalog, identifier, catalog_id=None):
         2. Distribuciones a parsear desde un TXT o CSV con formato de panel
     """
     distribution = catalog.get_distribution(identifier)
+    method = get_distribution_generation_method(distribution)
 
     # se genera a partir de un archivo de texto con parámetros
-    if not distribution.get("downloadURL") and _is_text_file(
-            distribution["scrapingFileURL"]):
+    if is_text_file or method == "text_file":
         return generate_ts_distribution_from_text_file(
             catalog, identifier, catalog_id)
 
     # se scrapea a partir de un Excel con parámetros (usando otro proyecto)
-    elif not distribution.get("downloadURL") and _is_excel_file(
-            distribution["scrapingFileURL"]):
+    elif is_excel_file or method == "excel_file":
         print("Usar el scraper de series de tiempo.")
         return None
 
     # se lee a partir de un CSV que cumple con la especificación
-    elif distribution.get("downloadURL"):
-        return pd.read_csv(distribution["downloadURL"],
-                           sep=",", encoding="utf-8")
+    elif is_csv_file or method == "csv_file":
+        file_source = file_source or distribution["downloadURL"]
+        time_index = get_distribution_time_index(distrib_meta)
+        return pd.read_csv(
+            file_source, index_col=time_index,
+            parse_dates=[time_index],
+            date_parser=lambda x: arrow.get(x, "YYYY-MM-DD").datetime
+            # encoding="utf-8"
+        )
 
     else:
         raise NotImplementedError("{} no se puede leer".format(identifier))
+
+
+def get_ts_distributions_by_method(catalog, method=None):
+    """Devuelve las dist. de series de tiempo generables por X método.
+
+    Args:
+        catalog (TimeSeriesDataJson): Catálogo con series de tiempo.
+        method (list or str): Lista o uno de 'text_file',
+            'csv_file', 'excel_file'
+    """
+    if not method:
+        return catalog.get_distributions(only_time_series=True)
+
+    # los métodos se pueden pasar por lista, o un solo método por string
+    distributions = []
+    if isinstance(method, str):
+        method = [method]
+
+    for distribution in catalog.get_distributions(only_time_series=True):
+        if get_distribution_generation_method(distribution) in method:
+            distributions.append(distribution)
+
+    return distributions
+
+
+def get_distribution_generation_method(distribution):
+    # se genera a partir de un archivo de texto con parámetros
+    if not distribution.get("downloadURL") and _is_text_file(
+            distribution["scrapingFileURL"]):
+        return "text_file"
+
+    # se scrapea a partir de un Excel con parámetros (usando otro proyecto)
+    elif (not distribution.get("downloadURL") and
+          _is_excel_file(distribution["scrapingFileURL"])):
+        return "excel_file"
+
+    # se lee a partir de un CSV que cumple con la especificación
+    elif distribution.get("downloadURL"):
+        return "csv_file"
+
+    else:
+        raise NotImplementedError("{} no se puede leer".format(
+            distribution["identifier"]))
 
 
 def _is_text_file(path_or_url):
