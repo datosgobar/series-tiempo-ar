1ca76413f4df4a0362b405515d8abc1f9b305b42
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2019-05-13 13:20:10 -0300

    Valido caso de distribucion con error

--- series_tiempo_ar/validator.py
+++ series_tiempo_ar/validator.py
@@ -1,2 +1,13 @@
-def get_distribution_errors(_catalog, _distribution):
-    return False
+from series_tiempo_ar import TimeSeriesDataJson
+from series_tiempo_ar.validations import validate_distribution
+
+
+def get_distribution_errors(catalog: TimeSeriesDataJson, distribution_id):
+    try:
+        distribution = catalog.get_distribution(distribution_id)
+        df = catalog.load_ts_distribution(distribution_id)
+        validate_distribution(df, catalog, None, distribution)
+    except Exception as e:
+        return [e]
+
+    return []
--- tests/test_errors.py
+++ tests/test_errors.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 
+from series_tiempo_ar.custom_exceptions import FieldIdRepetitionError
 from series_tiempo_ar.validator import get_distribution_errors
 from tests.helpers import read_data_json
 
@@ -9,3 +10,8 @@ class GetErrorsTests(TestCase):
         data_json = read_data_json("data.json")
         errors = get_distribution_errors(data_json, "125.1")
         self.assertFalse(errors)
+
+    def test_get_errors_repeated_field_id_catalog(self):
+        data_json = read_data_json("repeated_field_id.json")
+        errors = get_distribution_errors(data_json, "125.1")
+        self.assertIn(FieldIdRepetitionError, map(lambda x: x.__class__, errors))
