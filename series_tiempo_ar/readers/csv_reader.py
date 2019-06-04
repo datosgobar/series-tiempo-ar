13a2a5ce026da6d389149211bfe179b28c755d19
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2019-06-04 12:22:15 -0300

    Agrego lectura de distribuciones en latin1 en CSVReader

--- series_tiempo_ar/readers/csv_reader.py
+++ series_tiempo_ar/readers/csv_reader.py
@@ -1,10 +1,23 @@
+import io
+
 import pandas as pd
+import requests
 
 
 class CSVReader:
-    def __init__(self, distribution):
+    def __init__(self, distribution, verify_ssl=False):
         self.distribution = distribution
+        self.verify_ssl = verify_ssl
 
     def read(self):
         download_url = self.distribution["downloadURL"]
-        return pd.read_csv(download_url)
+
+        data = requests.get(download_url, verify=self.verify_ssl).content
+        file_source = io.BytesIO(data)
+
+        file_source.seek(0)
+        try:
+            return pd.read_csv(file_source, encoding="utf8")
+        except UnicodeDecodeError:
+            file_source.seek(0)
+            return pd.read_csv(file_source, encoding="latin1")
--- tests/test_readers/test_csv_reader.py
+++ tests/test_readers/test_csv_reader.py
@@ -11,3 +11,10 @@ class CSVReaderTests(TestCase):
         df = CSVReader(distribution).read()
 
         self.assertIn("title1", list(df.columns))
+
+    def test_read_latin1_distribution(self):
+        data_json = read_data_json("daily_periodicity_latin1.json")
+        distribution = data_json.get_distributions()[0]
+        df = CSVReader(distribution).read()
+
+        self.assertIn("tasas_interés_call", list(df.columns))
